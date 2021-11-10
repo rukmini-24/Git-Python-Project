6587dc9487e1d318ffdfd3b09fdb1197b00402ee
unknown <rukminigunda@gmail.com>
2021-11-10 10:32:15 +0530

    print statement was updated

--- main1.py
+++ main1.py
@@ -3,6 +3,6 @@ n=int(input("enter a number"))
 s=math.sqrt(n)
 r=int(s)
 if(s==r):
-    print("perfect square")
+    print("given number is a perfect square")
 else:
     print("not a perfect square")
